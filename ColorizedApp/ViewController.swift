c722341dc01878f11643fd55fcfebf9756bf37a4
Vladislav <vsuslov7@gmail.com>
2022-09-26 00:33:32 +0300

    Changed color creating method

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -21,12 +21,13 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+//    MARK: Private Properties
     private let colorComponents = ColorComponents()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         colorView.layer.cornerRadius = 20
-        colorView.backgroundColor = .black
+        colorView.backgroundColor = getCurrentColor()
         redSlider.tag = 1
         greenSlider.tag = 2
         setStartLabelValues()
@@ -45,22 +46,22 @@ class ViewController: UIViewController {
             colorComponents.blue = CGFloat(blueSlider.value)
             blueLabel.text = String(format: "%.1f", blueSlider.value)
         }
-        setupColor()
+        colorView.backgroundColor = getCurrentColor()
     }
 
     //MARK: Private Methods
-    
     private func setStartLabelValues() {
         redLabel.text = String(format: "%.1f", redSlider.value)
         greenLabel.text = String(format: "%.1f", greenSlider.value)
         blueLabel.text = String(format: "%.1f", blueSlider.value)
     }
     
-    private func setupColor() {
-        colorView.backgroundColor = UIColor(red: colorComponents.red,
-                                            green: colorComponents.green,
-                                            blue: colorComponents.blue,
-                                            alpha: 1)
+    private func getCurrentColor() -> UIColor {
+        let currentColor = UIColor(red: colorComponents.red,
+                               green: colorComponents.green,
+                               blue: colorComponents.blue,
+                               alpha: 1)
+        return currentColor
     }
 
 
