af1da3a93606a76ab6467592631bfc9e9721a691
Vladislav <vsuslov7@gmail.com>
2022-09-25 23:55:42 +0300

    Created change of colours with sliders

---
+++ ColorizedApp/ColorComponents.swift
@@ -0,0 +1,14 @@
+//
+//  ColorComponents.swift
+//  ColorizedApp
+//
+//  Created by Vladislav Suslov on 25.09.22.
+//
+
+import Foundation
+
+class ColorComponents {
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+}
--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,9 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    //MARK: IBOutlets
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    private let colorComponents = ColorComponents()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
+        colorView.backgroundColor = .black
+        setStartLabelValues()
+    }
+    
+    //MARK: IBActions
+    @IBAction func redSliderDidMoved(_ sender: UISlider) {
+        colorComponents.red = CGFloat(sender.value)
+        redLabel.text = String(format: "%.1f", sender.value)
+        setColor()
+    }
+    
+    @IBAction func greenSliderDidMoved(_ sender: UISlider) {
+        colorComponents.green = CGFloat(sender.value)
+        greenLabel.text = String(format: "%.1f", sender.value)
+        setColor()
+    }
+    
+    @IBAction func blueSliderDidMoved(_ sender: UISlider) {
+        colorComponents.blue = CGFloat(sender.value)
+        blueLabel.text = String(format: "%.1f", sender.value)
+        setColor()
+    }
+    
+    //MARK: Private Methods
+    
+    private func setStartLabelValues() {
+        redLabel.text = String(format: "%.1f", redSlider.value)
+        greenLabel.text = String(format: "%.1f", greenSlider.value)
+        blueLabel.text = String(format: "%.1f", blueSlider.value)
+    }
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(red: colorComponents.red,
+                                            green: colorComponents.green,
+                                            blue: colorComponents.blue,
+                                            alpha: 1)
     }
 
 
